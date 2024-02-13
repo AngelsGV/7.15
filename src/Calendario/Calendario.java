78103291c70c3f9ebeca5d33178bac6f807913bd
AngelsGV <angels.not99@gmail.com>
2024-02-14 00:21:22 +0100

    Initial commit

--- src/Calendario/Calendario.java
+++ src/Calendario/Calendario.java
@@ -1,27 +1,56 @@
 package Calendario;
 
 public class Calendario {
+    //Se crean los atributos necesarios para las clases
     int año;
     int mes;
     int dia;
-    public Calendario(int dia,int mes,int año){
+    //Se especifica que Calendario lleva esos atributos y se definen
+    public Calendario(){
         this.dia=dia;
         this.mes=mes;
         this.año=año;
 
     }
+    //gets y sets puestos todos para luego hacer comprobaciones con ellos
+    public int getAño() {
+        return año;
+    }
 
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    //Las funciones que me pide el ejercicio
     public void incrementarDia(){
+        //Los meses que tienen 31 días
         while (mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12){
             if(dia>0&&dia<=31){
                 dia++;
             }
         }
+        //El mes que tiene 29 dias
         while (mes==2){
             if(dia>0&&dia<=29){
                 dia++;
             }
         }
+        //Los meses que tienen 30  dias
         while (mes==4||mes==6||mes==9||mes==11){
             if(dia>0&&dia<=30){
                 dia++;
@@ -35,13 +64,12 @@ public class Calendario {
         }
 
     }
-    public void incrementarAño(){
+    public void incrementarAño(){//No cojemos años bisiestos
 
 
     }
     public void mostrar(){
-        System.out.println("Estamos a: " + dia + "/" + mes +"/"+
-                año);
+        System.out.println("La fecha introducida es: " + dia + "/" + mes +"/"+ año);
     }
 
     }
--- src/Calendario/Programa.java
+++ src/Calendario/Programa.java
@@ -1,8 +1,31 @@
 package Calendario;
 
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args){
 
+        //Ponemos un escaner porque pediremos al usuario introducir valores
+        Scanner sc = new Scanner(System.in);
+
+        //Creamos objeto calendario
+        Calendario calendario = new Calendario();
+
+        //Se pide por progama valores y con el set se introducen en el objeto calendario
+        System.out.println("Escribe el dia: ");
+        int d = sc.nextInt();
+         calendario.setDia(d);
+
+        System.out.println("Escriba el mes: ");
+        int m = sc.nextInt();
+        calendario.setMes(m);
+
+        System.out.println("Escribe el año: ");
+        int a = sc.nextInt();
+        calendario.setAño(a);
+
+        //Mostramos la fecha para ver que está correcta
+        calendario.mostrar();
 
     }
 }
