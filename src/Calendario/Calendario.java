8fc701fea99c836e77d26996f82fcf7e164ea0e4
AngelsGV <angels.not99@gmail.com>
2024-02-13 23:38:47 +0100

    Initial commit

---
+++ src/Calendario/Calendario.java
@@ -0,0 +1,26 @@
+package Calendario;
+
+public class Calendario {
+    int año;
+    int mes;
+    int dia;
+    public void incrementarDia(){
+
+
+    }
+    public void incrementarMes(){
+
+    }
+    public void incrementarAño(){
+
+    }
+    public void mostrar(){
+        System.out.println("Estamos a: " + dia + "/" + mes +"/"+
+                año);
+    }
+    public boolean iguales(Calendario otraFecha){
+
+    }
+
+    }
+
