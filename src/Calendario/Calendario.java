e284c59d5d11e0114669160eb1fbf9fef47b7655
AngelsGV <angels.not99@gmail.com>
2024-02-13 23:51:23 +0100

    Initial commit

--- src/Calendario/Calendario.java
+++ src/Calendario/Calendario.java
@@ -5,7 +5,21 @@ public class Calendario {
     int mes;
     int dia;
     public void incrementarDia(){
-
+        while (mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12){
+            if(dia>0&&dia<=31){
+                dia++;
+            }
+        }
+        while (mes==2){
+            if(dia>0&&dia<=29){
+                dia++;
+            }
+        }
+        while (mes==4||mes==6||mes==9||mes==11){
+            if(dia>0&&dia<=30){
+                dia++;
+            }
+        }
 
     }
     public void incrementarMes(){
