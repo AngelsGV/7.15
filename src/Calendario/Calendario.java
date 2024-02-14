06c2e8e326e0226a759a5251210ea25721cd5bd6
AngelsGV <angels.not99@gmail.com>
2024-02-14 18:14:26 +0100

    Initial commit

--- src/Calendario/Calendario.java
+++ src/Calendario/Calendario.java
@@ -76,23 +76,7 @@ public class Calendario {
 
     }
     public void incrementarAño() { // No cojemos años bisiestos
-
-        if (año != 0) {
-            int añoNuevo = año + añoIncrementar;
-
-            if (año < 0 && añoNuevo == 0) {
-                añoNuevo = 1;
-                año = añoNuevo;
-            } else if (año < 0 && añoNuevo > 0) {
-                añoNuevo += 1;
-                año = añoNuevo;
-            } else {
-                // Manejar el caso si año >= 0
-                año = añoNuevo;
-            }
-        } else {
-            System.out.println("No existe el año 0.");
-        }
+        año = año + añoIncrementar;
     }
 
 
@@ -102,4 +86,5 @@ public class Calendario {
     }
 
     }
-
+//----------------------------------------------------
+//1. Año no puede ser 0.
